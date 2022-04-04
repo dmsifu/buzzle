2755ab48432d2f895bd5fae32f080be6d9654572
00LX <laxus_z@hotmail.com>
2022-04-04 14:52:37 -0500

    added some styles to website

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Navbar from "./features/Nav/Navbar";
 import Tutorial from "./features/tutorial/Tutorial";
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <Tutorial/>
     </div>
   );
---
+++ src/features/Nav/Navbar.js
@@ -0,0 +1,13 @@
+import './Navbar.scss'
+
+function Navbar() {
+  return (
+    <ul className="navbar">
+        <li></li>
+        <h1>Buzzle</h1>
+        <li>Home</li>
+    </ul>
+  )
+}
+
+export default Navbar
